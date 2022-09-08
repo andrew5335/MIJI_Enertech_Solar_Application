face2747b7da065e7987d0973f20eea3d8d41b72
andrew <andrew2767@gmail.com>
2022-09-08 11:54:28 +0900

    블루투스 연동 처리 작업 진행 중

--- app/src/main/java/com/miji/solar/MijiMainActivity.java
+++ app/src/main/java/com/miji/solar/MijiMainActivity.java
@@ -1,5 +1,9 @@
 package com.miji.solar;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -10,23 +14,54 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.miji.solar.client.BluetoothSerialClient;
 import com.miji.solar.ui.main.SectionsPagerAdapter;
 import com.miji.solar.databinding.ActivityMijiMainBinding;
+
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class MijiMainActivity extends AppCompatActivity {
 
     private ActivityMijiMainBinding binding;
 
+    private BluetoothSerialClient bluetoothSerialClient;
+    private LinkedList<BluetoothDevice> mBluetoothDevices = new LinkedList<BluetoothDevice>();
+    private ArrayAdapter<String> mDeviceArrayAdapter;
+
+    private EditText mEditTextInput;
+    private TextView mTextView;
+    private Button mButtonSend;
+    private ProgressDialog mLoadingDialog;
+    private AlertDialog mDeviceListDialog;
+    private Menu mMenu;
+    private ListView listView;
+    private AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        bluetoothSerialClient = BluetoothSerialClient.getInstance();
+
+        initProgressDialog();
+        initDeviceListDialog();
 
         if (Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
@@ -43,6 +78,8 @@ public class MijiMainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+        ImageView bluetooth = binding.bluetooth;
+        mDeviceArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.item_device);
         //FloatingActionButton fab = binding.fab;
         /**
         fab.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +90,201 @@ public class MijiMainActivity extends AppCompatActivity {
             }
         });
         **/
+        bluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //initDeviceListDialog();
+                scanDevices();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        bluetoothSerialClient.cancelScan(getApplicationContext());
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableBluetooth();
+    }
+
+    private void enableBluetooth() {
+        BluetoothSerialClient btSet =  bluetoothSerialClient;
+        btSet.enableBluetooth(this, new BluetoothSerialClient.OnBluetoothEnabledListener() {
+            @Override
+            public void onBluetoothEnabled(boolean success) {
+                if(success) {
+                    getPairedDevices();
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void getPairedDevices() {
+        Set<BluetoothDevice> devices =  bluetoothSerialClient.getPairedDevices();
+        for(BluetoothDevice device: devices) {
+            addDeviceToArrayAdapter(device);
+        }
+    }
+
+    private void initProgressDialog() {
+        mLoadingDialog = new ProgressDialog(this);
+        mLoadingDialog.setCancelable(false);
+    }
+
+    private void initDeviceListDialog() {
+        listView = new ListView(getApplicationContext());
+        listView.setAdapter(mDeviceArrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item =  (String) parent.getItemAtPosition(position);
+                for(BluetoothDevice device : mBluetoothDevices) {
+                    if(item.contains(device.getAddress())) {
+                        connect(device);
+                        mDeviceListDialog.cancel();
+                    }
+                }
+            }
+        });
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select bluetooth device");
+        builder.setView(listView);
+        builder.setPositiveButton("Scan",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        scanDevices();
+                    }
+                });
+        mDeviceListDialog = builder.create();
+        //mDeviceListDialog.show();
+        mDeviceListDialog.setCanceledOnTouchOutside(false);
+    }
+
+    private void connect(BluetoothDevice device) {
+        mLoadingDialog.setMessage("Connecting....");
+        mLoadingDialog.setCancelable(false);
+        mLoadingDialog.show();
+        BluetoothSerialClient btSet =  bluetoothSerialClient;
+        btSet.connect(getApplicationContext(), device, mBTHandler);
+    }
+
+    private BluetoothSerialClient.BluetoothStreamingHandler mBTHandler = new BluetoothSerialClient.BluetoothStreamingHandler() {
+        ByteBuffer mmByteBuffer = ByteBuffer.allocate(1024);
+
+        @Override
+        public void onError(Exception e) {
+            Toast.makeText(getApplicationContext(), "connection error", Toast.LENGTH_LONG).show();
+            mLoadingDialog.cancel();
+            //addText("Messgae : Connection error - " +  e.toString() + "\n");
+            //mMenu.getItem(0).setTitle(R.string.action_connect);
+        }
+
+        @Override
+        public void onDisconnected() {
+            //mMenu.getItem(0).setTitle(R.string.action_connect);
+            Toast.makeText(getApplicationContext(), "disconnected", Toast.LENGTH_LONG).show();
+            mLoadingDialog.cancel();
+            //addText("Messgae : Disconnected.\n");
+        }
+        @Override
+        public void onData(byte[] buffer, int length) {
+            if(length == 0) return;
+            if(mmByteBuffer.position() + length >= mmByteBuffer.capacity()) {
+                ByteBuffer newBuffer = ByteBuffer.allocate(mmByteBuffer.capacity() * 2);
+                newBuffer.put(mmByteBuffer.array(), 0,  mmByteBuffer.position());
+                mmByteBuffer = newBuffer;
+            }
+            mmByteBuffer.put(buffer, 0, length);
+            if(buffer[length - 1] == '\0') {
+                try {
+                    //addText(bluetoothSerialClient.getConnectedDevice().getName() + " : " +
+                    //        new String(mmByteBuffer.array(), 0, mmByteBuffer.position()) + '\n');
+                    Toast.makeText(getApplicationContext(), bluetoothSerialClient.getConnectedDevice().getName() + " : " +
+                            new String(mmByteBuffer.array(), 0, mmByteBuffer.position()) + '\n', Toast.LENGTH_LONG).show();
+                    mmByteBuffer.clear();
+                } catch(SecurityException se) {
+                    se.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void onConnected() {
+            try {
+                //addText("Messgae : Connected. " + bluetoothSerialClient.getConnectedDevice().getName() + "\n");
+                Toast.makeText(getApplicationContext(), "connected : " + bluetoothSerialClient.getConnectedDevice().getName(), Toast.LENGTH_LONG).show();
+                mLoadingDialog.cancel();
+                //mMenu.getItem(0).setTitle(R.string.action_disconnect);
+            } catch(SecurityException se) {
+                se.printStackTrace();
+            }
+        }
+    };
+
+    private void addText(String text) {
+        mTextView.append(text);
+        final int scrollAmount = mTextView.getLayout().getLineTop(mTextView.getLineCount()) - mTextView.getHeight();
+        if (scrollAmount > 0)
+            mTextView.scrollTo(0, scrollAmount);
+        else
+            mTextView.scrollTo(0, 0);
+    }
+
+    private void addDeviceToArrayAdapter(BluetoothDevice device) throws SecurityException {
+        if(mBluetoothDevices.contains(device)) {
+            mBluetoothDevices.remove(device);
+            mDeviceArrayAdapter.remove(device.getName() + "\n" + device.getAddress());
+        }
+        mBluetoothDevices.add(device);
+        mDeviceArrayAdapter.add(device.getName() + "\n" + device.getAddress() );
+        mDeviceArrayAdapter.notifyDataSetChanged();
+
+    }
+
+    private void scanDevices() {
+        BluetoothSerialClient btSet = bluetoothSerialClient;
+        btSet.scanDevices(getApplicationContext(), new BluetoothSerialClient.OnScanListener() {
+            String message ="";
+            @Override
+            public void onStart() {
+                Log.d("Test", "Scan Start.");
+                mLoadingDialog.show();
+                message = "Scanning....";
+                mLoadingDialog.setMessage("Scanning....");
+                mLoadingDialog.setCancelable(true);
+                mLoadingDialog.setCanceledOnTouchOutside(true);
+                mLoadingDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        BluetoothSerialClient btSet = bluetoothSerialClient;
+                        btSet.cancelScan(getApplicationContext());
+                    }
+                });
+            }
+
+            @Override
+            public void onFoundDevice(BluetoothDevice bluetoothDevice) throws SecurityException {
+                addDeviceToArrayAdapter(bluetoothDevice);
+                message += "\n" + bluetoothDevice.getName() + "\n" + bluetoothDevice.getAddress();
+                mLoadingDialog.setMessage(message);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("Test", "Scan finish.");
+                message = "";
+                mLoadingDialog.cancel();
+                mLoadingDialog.setCancelable(false);
+                mLoadingDialog.setOnCancelListener(null);
+                initDeviceListDialog();
+                mDeviceListDialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/miji/solar/SplashActivity.java
+++ app/src/main/java/com/miji/solar/SplashActivity.java
@@ -2,38 +2,120 @@ package com.miji.solar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermissionUtil;
+import com.gun0912.tedpermission.normal.TedPermission;
+import com.miji.solar.client.BluetoothSerialClient;
+
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
+
+    private BluetoothSerialClient bluetoothSerialClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.custom));
+        // 블루투스 사용 가능 여부 확인
+        bluetoothSerialClient = BluetoothSerialClient.getInstance();
+        if (bluetoothSerialClient == null) {
+            Toast.makeText(getApplicationContext(), "블루투스 사용이 불가능한 기기입니다.", Toast.LENGTH_LONG).show();
         }
 
-        // splash 화면에서 2초뒤 main 화면으로 넘어가도록 설정
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent main = new Intent(getApplicationContext(), MijiMainActivity.class);
-                startActivity(main);
-                finish();
+        // 권한 적용 여부 확인
+        boolean isGranted = TedPermissionUtil.isGranted(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                , Manifest.permission.READ_EXTERNAL_STORAGE
+                , Manifest.permission.ACCESS_WIFI_STATE
+                , Manifest.permission.CHANGE_WIFI_STATE
+                , Manifest.permission.READ_PHONE_STATE
+                , Manifest.permission.ACCESS_FINE_LOCATION
+                , Manifest.permission.BLUETOOTH_CONNECT
+                , Manifest.permission.BLUETOOTH_SCAN
+                , Manifest.permission.BLUETOOTH_ADMIN
+                , Manifest.permission.BLUETOOTH_ADVERTISE
+                , Manifest.permission.BLUETOOTH);
+
+        if(isGranted) {
+
+            if (Build.VERSION.SDK_INT >= 21) {
+                Window window = this.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.setStatusBarColor(this.getResources().getColor(R.color.custom));
             }
-        }, 2000);
+
+            // splash 화면에서 2초뒤 main 화면으로 넘어가도록 설정
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent main = new Intent(getApplicationContext(), MijiMainActivity.class);
+                    startActivity(main);
+                    finish();
+                }
+            }, 2000);
+        } else {
+            PermissionListener permissionlistener = new PermissionListener() {
+                @Override
+                public void onPermissionGranted() {
+                    Toast.makeText(SplashActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // 권한이 허용된 경우 메인 화면으로 이동
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent main = new Intent(getApplicationContext(), MijiMainActivity.class);
+                            startActivity(main);
+                            finish();
+                        }
+                    }, 1000);
+                }
+
+                @Override
+                public void onPermissionDenied(List<String> deniedPermissions) {
+                    Toast.makeText(SplashActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT)
+                            .show();
+                    finish();
+                }
+
+
+            };
+
+            TedPermission.create()
+                    .setPermissionListener(permissionlistener)
+                    .setRationaleTitle("권한 허용 요청")
+                    .setRationaleMessage("앱 이용을 위해서는 권한 허용이 필요합니다.")
+                    .setDeniedTitle("권한 허용 거부")
+                    .setDeniedMessage(
+                            "앱 이용에 필요한 권한을 허용하지 않으실 경우 앱 사용이 불가능합니다.\n\n 권한을 허용해주세요.")
+                    .setGotoSettingButtonText("권한 설정")
+                    .setPermissions(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            , Manifest.permission.READ_EXTERNAL_STORAGE
+                            , Manifest.permission.ACCESS_WIFI_STATE
+                            , Manifest.permission.CHANGE_WIFI_STATE
+                            , Manifest.permission.READ_PHONE_STATE
+                            , Manifest.permission.ACCESS_FINE_LOCATION
+                            , Manifest.permission.BLUETOOTH_CONNECT
+                            , Manifest.permission.BLUETOOTH_SCAN
+                            , Manifest.permission.BLUETOOTH_ADMIN
+                            , Manifest.permission.BLUETOOTH_ADVERTISE
+                            , Manifest.permission.BLUETOOTH)
+                    .check();
+        }
 
     }
 }
