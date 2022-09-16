6d0d992218b5513bf9f76c4bd8595f7385c456a8
andrew <andrew2767@gmail.com>
2022-09-16 10:09:36 +0900

    viewmodel 구성 진행 중

---
+++ app/src/main/java/com/miji/solar/model/Data.java
@@ -0,0 +1,14 @@
+package com.miji.solar.model;
+
+public class Data {
+
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
